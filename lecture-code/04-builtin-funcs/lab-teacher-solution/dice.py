fe7b482c2e4ad6a0220ffb1401840f8f42a1fe78
P Campbell <pcampbell.edu@gmail.com>
2022-02-06 17:12:16 -0500

    Create dice.py

---
+++ lecture-code/04-builtin-funcs/lab-teacher-solution/dice.py
@@ -0,0 +1,52 @@
+# exercises using the math library
+# 04-02 using random - dice
+# pmcampbell
+# 2022-02-06
+
+# imports below
+import random
+
+roll1 = random.randint(1,6)
+roll2 = random.randint(1,6)
+print(f' first roll {roll1} {roll2}')
+
+roll1 = random.randint(1,6)
+roll2 = random.randint(1,6)
+print(f' 2nd roll {roll1} {roll2}')
+
+roll1 = random.randint(1,6)
+roll2 = random.randint(1,6)
+print(f' 3rd roll {roll1} {roll2}')
+
+roll1 = random.randint(1,6)
+roll2 = random.randint(1,6)
+print(f' 4th roll {roll1} {roll2}')
+
+roll1 = random.randint(1,6)
+roll2 = random.randint(1,6)
+print(f' 5th roll {roll1} {roll2}')
+
+seed = int(input("seed: "))
+print(f'seed {seed}')
+
+random.seed(seed)
+
+roll1 = random.randint(1,6)
+roll2 = random.randint(1,6)
+print(f' first roll {roll1} {roll2}')
+
+roll1 = random.randint(1,6)
+roll2 = random.randint(1,6)
+print(f' 2nd roll {roll1} {roll2}')
+
+roll1 = random.randint(1,6)
+roll2 = random.randint(1,6)
+print(f' 3rd roll {roll1} {roll2}')
+
+roll1 = random.randint(1,6)
+roll2 = random.randint(1,6)
+print(f' 4th roll {roll1} {roll2}')
+
+roll1 = random.randint(1,6)
+roll2 = random.randint(1,6)
+print(f' 5th roll {roll1} {roll2}')
