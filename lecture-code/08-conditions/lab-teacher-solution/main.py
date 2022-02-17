05c00a312875f49a38d9c4cee36a76d4afdfccef
P Campbell <pcampbell.edu@gmail.com>
2022-02-17 09:16:48 -0500

    Update main.py

--- lecture-code/08-conditions/lab-teacher-solution/main.py
+++ lecture-code/08-conditions/lab-teacher-solution/main.py
@@ -1,9 +1,20 @@
-# the usual stuff here (all names if team work)
+# given a year, month, day determine the day of the week
+# using zellers congruence algorithm
+# pmcampbell
+# 2022-02-14
 
-#code the zeller function below
+'''
+implement the zellers congruence algorithm
+params:   year, month, day
+returns:  numeric day of week 0-6
+
+(note y,c,m OKish ariable names, used for speed's sake taken directly from the formula given
+test case
+Feb 1, 1998 results in Sunday
+'''
 def zeller(year, month, day):
-  y = year//100
-  c = year %  100
+  y = year//100  # last 2 digits of year
+  c = year %  100  # first 2 digits of the year
   m = month -2
   if m < 1:
     m = m - 1
@@ -33,14 +44,14 @@ def day_name(day_number):
   return day
 
 #code the main function below
-
+'''
+main code drives the functions
+'''
 def main():
   year = int(input("enter a year: "))  
   month = int(input("enter a month: "))  
   day = int(input("enter a day: "))  
   print(f"That day is/was/will be a {day_name(zeller(year,  month, day))}")
-
-
 
 # Code below ensures that the function called main is invoked when you run
 if __name__ == "__main__":
